ab325476687b7cf515088b0edde7b212c74e2f5e
Panoschi Cristian <pan.cristian01@gmail.com>
2024-02-01 17:45:32 +0200

    Made it possible to move between nodes on the map

--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -1,33 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    //view height: 10 --- 4(2 up, 2 down) clear on the edges
-    //view width: 18
-    private int currentStage;
-    private List<MapNode> nodes;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public GameObject currentStage;
+    private Camera _mainCamera;
+    void Awake() 
     {
-        nodes = new List<MapNode>();
-        currentStage = 0;
-
-        List<int> successors = new List<int>(){1,2,3};
-        Vector3 coordinates = new Vector3(-7.5f, 0, 0);
-        MapNode startNode = new MapNode(coordinates, 0, 0, successors);
-        nodes.Add(startNode);
-        nodes[0].printHelloWorld();
+        _mainCamera = Camera.main;
     }
 
-    private void populateMap()
-    {
-
-    }
     // Update is called once per frame
     void Update()
     {
-        
+        player.transform.position = currentStage.transform.position + new Vector3 (0, 0.5f, -1);
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if(!context.started) return;
+
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        if(!rayHit.collider) return;
+
+        Debug.Log(rayHit.collider.gameObject.name);
+        foreach(GameObject iteratorGameObject in currentStage.GetComponent<MapNode>().successors)
+        {
+            if(rayHit.collider.gameObject.name == iteratorGameObject.name)
+            {
+                currentStage = iteratorGameObject;
+            }
+        }
     }
 }
--- Assets/Scripts/Map/MapNode.cs
+++ Assets/Scripts/Map/MapNode.cs
@@ -2,22 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapNode
+public class MapNode : MonoBehaviour
 {
-    private bool hasPlayer;
-    private Vector3 coordinates;
-    private int stageNr;
-    private int lineNr;
-    private List<int> successorsNr;
-
-    public MapNode(Vector3 coordinates, int stageNr, int lineNr, List<int> successors)
-    {
-        hasPlayer = false;
-        this.coordinates = coordinates;
-        this.stageNr = stageNr;
-        this.lineNr = lineNr;
-        this.successorsNr = new List<int>(successors);
-    }
+    public bool hasPlayer;
+    public List<GameObject> successors;
     
     public void printHelloWorld()
     {
