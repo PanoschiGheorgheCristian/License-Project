8e77fb09cd09ad7b5cf699f88954523e310c427a
Panoschi Cristian <pan.cristian01@gmail.com>
2024-02-02 15:08:20 +0200

    Finished the start of map scene, refactored some code

--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 
 public class MapController : MonoBehaviour
 {
+    private bool isSceneLoading;
     public GameObject player;
     public GameObject currentStage;
     private Camera _mainCamera;
+    [SerializeField] private List<GameObject> mapNodes;
     void Awake() 
     {
         _mainCamera = Camera.main;
+        isSceneLoading = false;
+        SetEncounters();
     }
 
     // Update is called once per frame
     void Update()
     {
-        player.transform.position = currentStage.transform.position + new Vector3 (0, 0.5f, -1);
+        
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -27,12 +33,72 @@ public class MapController : MonoBehaviour
         if(!rayHit.collider) return;
 
         Debug.Log(rayHit.collider.gameObject.name);
-        foreach(GameObject iteratorGameObject in currentStage.GetComponent<MapNode>().successors)
-        {
-            if(rayHit.collider.gameObject.name == iteratorGameObject.name)
+        if(isSceneLoading == false)
+            foreach(GameObject iteratorGameObject in currentStage.GetComponent<MapNode>().successors)
             {
-                currentStage = iteratorGameObject;
+                if(rayHit.collider.gameObject.name == iteratorGameObject.name)
+                {
+                    currentStage.GetComponent<MapNode>().hasPlayer = false;
+                    currentStage = iteratorGameObject;
+                    currentStage.GetComponent<MapNode>().hasPlayer = true;
+                    player.transform.position = currentStage.transform.position + new Vector3 (0, 0.5f, -1);
+                    isSceneLoading = true;
+                    StartCoroutine(LoadStage());
+                }
             }
+    }
+
+    private void SetEncounters()
+    {
+        mapNodes[0].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Start);
+        mapNodes[^1].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Boss);
+
+        float rand = UnityEngine.Random.Range(0f,2f);
+        if(rand < 1f)
+        {
+            mapNodes[7].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.HardEnemy);
+            mapNodes[8].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Camp);
+            mapNodes[^2].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.HardEnemy);
+            mapNodes[^3].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Camp);
+        }
+        else
+        {
+            mapNodes[7].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Camp);
+            mapNodes[8].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.HardEnemy);
+            mapNodes[^2].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Camp);
+            mapNodes[^3].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.HardEnemy);
+        }
+        for(int index = 1; index <= mapNodes.Count - 2; index++)
+        {
+            if(index % 8 == 7)  
+            {
+                index += 1;
+                continue;
+            }
+            rand = UnityEngine.Random.Range(0f,3f);
+            if(rand < 1f)
+                mapNodes[index].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Enemy);
+            else    if(rand < 2f)
+                        mapNodes[index].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Treasure);
+                    else    mapNodes[index].GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Event);
+            // node.GetComponent<MapNode>().SetEncounter(MapNode.Encounter.Enemy);
+        }
+    }
+
+    private IEnumerator LoadStage()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        isSceneLoading = false;
+        if(this.currentStage.GetComponent<MapNode>().encounter == MapNode.Encounter.Enemy || 
+            this.currentStage.GetComponent<MapNode>().encounter == MapNode.Encounter.HardEnemy ||
+                this.currentStage.GetComponent<MapNode>().encounter == MapNode.Encounter.Boss)
+        {
+            SceneManager.LoadScene("Combat", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene("Encounter", LoadSceneMode.Single);
         }
     }
 }
--- Assets/Scripts/Map/MapNode.cs
+++ Assets/Scripts/Map/MapNode.cs
@@ -1,19 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapNode : MonoBehaviour
 {
+    public enum Encounter {Start, Enemy, HardEnemy, Camp, Treasure, Event, Boss};
+    public Encounter encounter;
     public bool hasPlayer;
     public List<GameObject> successors;
-    
-    public void printHelloWorld()
-    {
-        Debug.Log("Hello World!");
-    }
     // Update is called once per frame
     void Update()
     {
-        
+        //move event picture around
+    }
+
+    public void SetEncounter(Encounter encounter)
+    {
+        this.encounter = encounter;
     }
 }
