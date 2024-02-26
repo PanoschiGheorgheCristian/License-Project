19057239b65fd7e2a193b189a6f82add3ff29185
Panoschi Cristian <pan.cristian01@gmail.com>
2024-02-26 12:52:43 +0200

    Made a very basic encounter menu

---
+++ Assets/Scripts/Encounter/ButtonActions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions : MonoBehaviour
+{
+    public void PrintStuff()
+    {
+        Debug.Log("ButtonPress");
+    }
+}
