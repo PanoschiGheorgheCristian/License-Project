33040e08f4b576df59354b22d4f8370ee9c975c7
Panoschi Cristian <pan.cristian01@gmail.com>
2024-03-26 15:24:14 +0200

    Made encounter buttons load the map

--- Assets/Scripts/Encounter/ButtonActions.cs
+++ Assets/Scripts/Encounter/ButtonActions.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class ButtonActions : MonoBehaviour
 {
-    public void PrintStuff()
+    public void LoadMap()
     {
-        Debug.Log("ButtonPress");
+        Debug.Log("Loading map...");
+        StartCoroutine(LoadMapDelay());
+    }
+
+    private IEnumerator LoadMapDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene("Map", LoadSceneMode.Single); 
     }
 }
